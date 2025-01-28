1ef0acd68a52ed2acba7a74f932f65707c809b64
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2025-01-28 21:37:54 +0530

    Filters added asOn 28/1/25 by NS

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,4 +1,18 @@
-import { branches, cities } from "@/constants";
+import {
+  be,
+  branches,
+  brand,
+  brandform,
+  broadChannel,
+  category,
+  channel,
+  months,
+  shortChannel,
+  sm,
+  sub_brandform,
+  years,
+  zm,
+} from "@/constants";
 import FilterDropdown from "./FilterDropdown";
 import DateRangeFilter from "./DateRangeFilter";
 
@@ -15,9 +29,20 @@ const Header = () => {
 
       {/* FILTERS */}
       <div className="flex items-center gap-2">
+        <FilterDropdown filter={years} name="Year" />
+        <FilterDropdown filter={months} name="Month" />
         <FilterDropdown filter={branches} name="Branch" />
-        <FilterDropdown filter={cities} name="City" />
-        <DateRangeFilter name="Date Range" />
+        <FilterDropdown filter={zm} name="ZM" />
+        <FilterDropdown filter={sm} name="SM" />
+        <FilterDropdown filter={be} name="BE" />
+        <FilterDropdown filter={channel} name="Channel" />
+        <FilterDropdown filter={broadChannel} name="Broad Channel" />
+        <FilterDropdown filter={shortChannel} name="Short Channel" />
+        <FilterDropdown filter={category} name="Category" />
+        <FilterDropdown filter={brand} name="Brand" />
+        <FilterDropdown filter={brandform} name="Brandform" />
+        <FilterDropdown filter={sub_brandform} name="Sub_Brandform" />
+        {/* <DateRangeFilter name="Date Range" /> */}
       </div>
     </div>
   );
