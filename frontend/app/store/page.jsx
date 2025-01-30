e5d2720a624e5fee40588cf80667dced1634f453
Writ2003 <dassupratim57@gmail.com>
2025-01-30 21:48:36 +0530

    Store Page Added_as_on_30/01/2025_by_SD

--- frontend/app/store/page.jsx
+++ frontend/app/store/page.jsx
@@ -1,5 +1,193 @@
+"use client"
+import { diagonal_arrow, filter } from "@/public/assets/Images";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 const Store = () => {
-  return <div>Store page</div>;
+  const [storeList, setStoreList] = useState([
+    {
+      "id":1,
+      "Old Code":"GAGA_01098",
+      "New Code":"GAGA_01098",
+      "New Branch":"North",
+      "DSE Code":"BGAGA_MS001",
+      "ZM":"Somnath Banerjee",
+      "SM":"Biswajit Bardhan",
+      "BE":"Biswajit Bardhan",
+      "STL":"Avijit Das"
+    },
+    {
+      "id":2,
+      "Old Code":"GAGA_01098",
+      "New Code":"GAGA_01098",
+      "New Branch":"North",
+      "DSE Code":"BGAGA_MS001",
+      "ZM":"Somnath Banerjee",
+      "SM":"Biswajit Bardhan",
+      "BE":"Biswajit Bardhan",
+      "STL":"Avijit Das"
+    },
+    {
+      "id":3,
+      "Old Code":"GAGA_01098",
+      "New Code":"GAGA_01098",
+      "New Branch":"North",
+      "DSE Code":"BGAGA_MS001",
+      "ZM":"Somnath Banerjee",
+      "SM":"Biswajit Bardhan",
+      "BE":"Biswajit Bardhan",
+      "STL":"Avijit Das"
+    },
+    {
+      "id":4,
+      "Old Code":"GAGA_01098",
+      "New Code":"GAGA_01098",
+      "New Branch":"North",
+      "DSE Code":"BGAGA_MS001",
+      "ZM":"Somnath Banerjee",
+      "SM":"Biswajit Bardhan",
+      "BE":"Biswajit Bardhan",
+      "STL":"Avijit Das"
+    },
+    {
+      "id":5,
+      "Old Code":"GAGA_01098",
+      "New Code":"GAGA_01098",
+      "New Branch":"North",
+      "DSE Code":"BGAGA_MS001",
+      "ZM":"Somnath Banerjee",
+      "SM":"Biswajit Bardhan",
+      "BE":"Biswajit Bardhan",
+      "STL":"Avijit Das"
+    },
+    {
+      "id":6,
+      "Old Code":"GAGA_01098",
+      "New Code":"GAGA_01098",
+      "New Branch":"North",
+      "DSE Code":"BGAGA_MS001",
+      "ZM":"Somnath Banerjee",
+      "SM":"Biswajit Bardhan",
+      "BE":"Biswajit Bardhan",
+      "STL":"Avijit Das"
+    },
+    {
+      "id":7,
+      "Old Code":"GAGA_01098",
+      "New Code":"GAGA_01098",
+      "New Branch":"North",
+      "DSE Code":"BGAGA_MS001",
+      "ZM":"Somnath Banerjee",
+      "SM":"Biswajit Bardhan",
+      "BE":"Biswajit Bardhan",
+      "STL":"Avijit Das"
+    },
+    {
+      "id":8,
+      "Old Code":"GAGA_01098",
+      "New Code":"GAGA_01098",
+      "New Branch":"North",
+      "DSE Code":"BGAGA_MS001",
+      "ZM":"Somnath Banerjee",
+      "SM":"Biswajit Bardhan",
+      "BE":"Biswajit Bardhan",
+      "STL":"Avijit Das"
+    },
+    {
+      "id":9,
+      "Old Code":"GAGA_01098",
+      "New Code":"GAGA_01098",
+      "New Branch":"North",
+      "DSE Code":"BGAGA_MS001",
+      "ZM":"Somnath Banerjee",
+      "SM":"Biswajit Bardhan",
+      "BE":"Biswajit Bardhan",
+      "STL":"Avijit Das"
+    },
+    {
+      "id":10,
+      "Old Code":"GAGA_01098",
+      "New Code":"GAGA_01098",
+      "New Branch":"North",
+      "DSE Code":"BGAGA_MS001",
+      "ZM":"Somnath Banerjee",
+      "SM":"Biswajit Bardhan",
+      "BE":"Biswajit Bardhan",
+      "STL":"Avijit Das"
+    },
+  ]);
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const [searchedStores,setSearchedStores] = useState([]);
+
+  const filteredItems = searchedStores.filter((store) =>
+    store.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleSearchInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  }
+  return (
+    <div className="pt-3 mx-5">
+      <div className="grid grid-cols-5">
+        <div className="flex-col col-span-1">
+          <div className="text-3xl font-bold">Store List</div>
+          <div className="text-md font-semibold text-gray-500 tracking-wide">Tabular view of Stores</div>
+        </div>
+        <div className="col-span-3 flex justify-center items-center relative">
+          <div className="w-5/6">
+            <input className="px-2 py-1.5 border-black border-2 outline-none rounded-full w-full" placeholder="Search here" value={searchQuery} onChange={handleSearchInputChange}></input>
+          </div>
+          <ul className="z-30 absolute top-24 max-h-44 overflow-y-auto scrollbar-hide md:top-16 lg:top-14 left-50 rounded-xl bg-slate-100 shadow-md shadow-gray-700 w-5/6">
+            <li className="border-b-slate-300 border-b py-2 px-4 cursor-pointer">↖ hello world</li>
+            <li className="border-b-slate-300 border-b py-2 px-4">↖ hello world</li>
+            <li className="border-b-slate-300 border-b py-2 px-4">↖ hello world</li>
+            <li className="border-b-slate-300 border-b py-2 px-4">↖ hello world</li>
+            <li className="border-b-slate-300 border-b py-2 px-4">↖ hello world</li>
+            <li className=" py-2 px-4">↖ hello world</li>
+          </ul>
+        </div>
+        <div className="col-span-1 flex justify-center items-center">
+          <div className="border-2 border-black px-1 py-2 rounded-md cursor-pointer">
+            <div className="flex gap-2">
+              <Image src={filter} height={20} width={20} alt="filter"/>
+              <div className="leading-5 tracking-wider">Filter</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-8 flex justify-center ">
+        <table className="table-fixed border-separate border-black border-2 w-full rounded-t-md max-h-screen overflow-y-auto">
+          <thead className="sticky top-0">
+            <tr>
+              <th className="w-1/12 border-r-2 border-b-2 border-black bg-green-200">Old Code</th>
+              <th className="w-1/12 border-r-2 border-b-2 border-black bg-white">New Code</th>
+              <th className="w-1/12 border-r-2 border-b-2 border-black bg-green-200">New Branch</th>
+              <th className="w-1/12 border-r-2 border-b-2 border-black bg-white">DSE Code</th>
+              <th className="w-2/12 border-r-2 border-b-2 border-black bg-green-200">ZM</th>
+              <th className="w-2/12 border-r-2 border-b-2 border-black bg-white">SM</th>
+              <th className="w-2/12 border-r-2 border-b-2 border-black bg-green-200">BE</th>
+              <th className="w-2/12 border-b-2 border-black bg-white">STL</th>
+            </tr>
+          </thead>
+          {storeList.map((store,index)=>(
+              <tbody key={store["id"]} >
+                <tr>
+                  <td className={`w-1/12 border-r-2 border-black ${(storeList.length-1)===index?"":"border-b-2 "} text-center`}>{store["Old Code"]}</td>
+                  <td className={`w-1/12 border-r-2 border-black ${(storeList.length-1)===index?"":"border-b-2 "} text-center`}>BGAGA_01098</td>
+                  <td className={`w-1/12 border-r-2 border-black ${(storeList.length-1)===index?"":"border-b-2 "} text-center`}>NORTH</td>
+                  <td className={`w-1/12 border-r-2 border-black ${(storeList.length-1)===index?"":"border-b-2 "} text-center`}>BGAGA_MS001</td>
+                  <td className={`w-1/12 border-r-2 border-black ${(storeList.length-1)===index?"":"border-b-2 "} text-center`}>Somnath Banerjee</td>
+                  <td className={`w-1/12 border-r-2 border-black ${(storeList.length-1)===index?"":"border-b-2 "} text-center`}>Biswajit Bardhan</td>
+                  <td className={`w-1/12 border-r-2 border-black ${(storeList.length-1)===index?"":"border-b-2 "} text-center`}>Biswajit Bardhan</td>
+                  <td className={`w-2/12 ${(storeList.length-1)===index?"":"border-b-2 border-black"} text-center`}>Avijit Das</td>
+                </tr>
+              </tbody>))}
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Store;
---
+++ frontend/public/assets/Images.js
@@ -0,0 +1,4 @@
+import filter from "./icons8-filter-50.png"
+import diagonal_arrow from "./diagonal.png"
+
+export {filter,diagonal_arrow}
