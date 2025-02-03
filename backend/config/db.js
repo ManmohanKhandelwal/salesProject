2ee07f4466f4cbfa4db44351e463f62553856baa
Manmohan Khandelwal <kmanmohan925@gmail.com>
2025-02-03 20:29:42 +0530

    Integration_of_AWS-RDS_as_on_03.02.2025_by_MK

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
 import mysql from "mysql2/promise";
-
+import dotenv from "dotenv";
+dotenv.config();
+//Connect to the database
 const mySqlPool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Nilanjan@12345",
-  port: 3306,
-  database: "sales_db",
+  host: process.env.DB_AWS_RDS_ENDPOINT,
+  user: process.env.DB_RDS_USERNAME,
+  password: process.env.DB_RDS_PASSWORD,
+  port: process.env.DB_RDS_PORT,
+  database: process.env.DB_RDS_DATABASE,
 });
 
 export default mySqlPool;
