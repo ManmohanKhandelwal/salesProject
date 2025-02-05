d7c15ed783066762a52e11aeeda414219ca88be4
Manmohan Khandelwal <kmanmohan925@gmail.com>
2025-02-05 20:01:21 +0530

    Concept_of_Onchange-Frontend;Backend_Cmd_Update_as_on_05.02.2025_by_MK

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   be,
   branches,
@@ -15,8 +16,39 @@ import {
 } from "@/constants";
 import FilterDropdown from "./FilterDropdown";
 import DateRangeFilter from "./DateRangeFilter";
+import { useState } from "react";
+
+//Array to run Loop to show all filters which are imported from constants
+const filtersToShow = [
+  { filterModule: years, filterLabel: "Year" },
+  { filterModule: months, filterLabel: "Month" },
+  { filterModule: branches, filterLabel: "Branch" },
+  { filterModule: zm, filterLabel: "ZM" },
+  { filterModule: sm, filterLabel: "SM" },
+  { filterModule: be, filterLabel: "BE" },
+  { filterModule: channel, filterLabel: "Channel" },
+  { filterModule: broadChannel, filterLabel: "Broad Channel" },
+  { filterModule: shortChannel, filterLabel: "Short Channel" },
+  { filterModule: category, filterLabel: "Category" },
+  { filterModule: brand, filterLabel: "Brand" },
+  { filterModule: brandform, filterLabel: "Brandform" },
+  { filterModule: sub_brandform, filterLabel: "Sub_Brandform" },
+];
 
 const Header = () => {
+
+  //UseState to store the selected filters
+  const [selectedFilters, setSelectedFilters] = useState({
+    //Set the default values of filters
+  });
+  const submitForm = () => {
+    console.log(selectedFilters);
+    //fetch api url with selected filters
+    //also pass the parent graphical states here as props to update the data and show it.
+  //Why the Filterdropdown is not having onchange attribute due to usage of external libs
+  // so make them such that on change it should save values in usestate->selectedFilters
+  // onsubmit buttons it should use that
+  };
   return (
     <div className="flex items-center justify-between">
       {/* HEADING */}
@@ -29,7 +61,7 @@ const Header = () => {
 
       {/* FILTERS */}
       <div className="flex items-center gap-1 flex-wrap">
-        <FilterDropdown filter={years} name="Year" />
+        {/* <FilterDropdown filter={years} name="Year" />
         <FilterDropdown filter={months} name="Month" />
         <FilterDropdown filter={branches} name="Branch" />
         <FilterDropdown filter={zm} name="ZM" />
@@ -41,9 +73,23 @@ const Header = () => {
         <FilterDropdown filter={category} name="Category" />
         <FilterDropdown filter={brand} name="Brand" />
         <FilterDropdown filter={brandform} name="Brandform" />
-        <FilterDropdown filter={sub_brandform} name="Sub_Brandform" />
+        <FilterDropdown filter={sub_brandform} name="Sub_Brandform" /> */}
+        {filtersToShow.map((filter, index) => (
+          <FilterDropdown
+            key={index}
+            filter={filter.filterModule}
+            name={filter.filterLabel}
+            // onChanges={(selected) => {
+            //   setSelectedFilters({ ...selectedFilters, [filter.filterLabel]: selected });
+            // }}
+          />
+        ))}
         {/* <DateRangeFilter name="Date Range" /> */}
       </div>
+      {/* Submits the form */}
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={submitForm}>
+        Submit
+      </button>
     </div>
   );
 };
