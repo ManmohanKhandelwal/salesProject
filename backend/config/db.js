8924618e4f1f8f1a9b92a5c1d5e299b187fc758f
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2025-02-10 20:09:33 +0530

    Setting db for localhost asOn 10/2/25 by NS

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 //Connect to the database
 const mySqlPool = mysql.createPool({
-  host: process.env.DB_AWS_RDS_ENDPOINT,
-  user: process.env.DB_RDS_USERNAME,
-  password: process.env.DB_RDS_PASSWORD,
-  port: process.env.DB_RDS_PORT,
-  database: process.env.DB_RDS_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
   connectTimeout: 60000,
 });
 
